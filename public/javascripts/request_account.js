e081d4565c4835ca6474095c9d260f56c3edde53
dima <shifting.planes@gmail.com>
2016-08-13 16:31:55 -0400

    signup form added

--- public/javascripts/request_account.js
+++ public/javascripts/request_account.js
@@ -1,3 +1,8 @@
+// Register email validator function.
+Vue.validator('user_email', function (val) {
+  return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(val)
+})
+
 new Vue({
   el: '#request_account_fields',
   //refactor later to use a template
@@ -5,32 +10,53 @@ new Vue({
     account_request: {
       user_name:'',
       user_email: '',
-      referral_source: '...',
+      referral_source: null,
       email_signup: false
     },
-    message_to_user: null
+    failed_user_signup: false,
   },
 
   methods:{
     sendData: function(){
       //console.log("send button pressed");
+      if(this.account_request.referral_source===null){
+        this.account_request.referral_source='none';
+      };
 
       this.$http.post('/request-account', this.account_request).then((response) => {
-          console.log(response.json().status);
-          this.your_name= response.json().name;
+          //console.log(response.json().status);
+          window.location.href= response.json().redirect_to;
         }, (response) => {
           // error callback
-          console.log("was not able to send data");
+          this.failed_user_signup=true;
+          //console.log(response.json().status);
+          window.location.href= response.json().redirect_to;
       });
     }
   },
   computed: {
     activeButton: function(){
-      if((this.account_request.user_name.length>1)&&(this.account_request.user_email.length>1))
+      if((this.account_request.user_name.length>1)&&(this.account_request.user_email.length>1)&&(!this.$validation1.address.user_email))
       {
         return false;
       }
       else{
+        return true;
+      }
+    },
+    trackEmailValue: function(){
+      if(this.$validation1.address){
+        return !this.$validation1.address.user_email;
+      }
+      else {
+        return false;
+      }
+    },
+    trackNameValue: function(){
+      if(this.$validation1.username){
+        return this.$validation1.username.valid;
+      }
+      else {
         return true;
       }
     }
--- routes/index.js
+++ routes/index.js
@@ -25,58 +25,72 @@ router.get('/signup',
 //yuck! this is so getting refactored
 router.post('/request-account',
   function(req, res, next){
-    console.log(req.body);
-    // //validate the params
-    // req.checkBody("name", "Please enter a valid name.").isLength({min:1,max:50});
-    // req.checkBody("email", "Please enter a valid email address.").isEmail();
-    // req.checkBody("referral_source", "Something strange with the referral source").optional().isLength({min:1,max:150});
-    //
-    // var errors = req.validationErrors();
-    // //redirect to confirmation page or kick out an error
-    // if (errors) {
-    //   res.json(errors);
-    //   //return;
-    // } else {
-    //   //sanitize the submitted params and push them to a google spreadsheet
-    //   var name = sanitizer.sanitize(req.body.name);
-    //   var email = req.body.email;
-    //   var referral_source = sanitizer.sanitize(req.body.referral_source);
-    //   //some basic injection protection need to learn more about this topic
-    //   referral_source = referral_source.replace("$","DOLLAR_SIGN");
-    //   var text_to_email = name + " at "+ email+" requests an invite.\n Referral source is: "+ referral_source;
-    //   var account_request = {'subject':'Requesting an Account Invite','text':text_to_email};
-    //
-    //   //email reminder to admin
-    //   //mailer.sendAdminEmail(account_request);
-    //
-    //   //log to database
-    //   MongoClient.connect(process.env.LINK_TO_MONGO, function(err, database){
-    //     if (err){
-    //       // console.log(err);
-    //       next(err);
-    //     }
-    //     else{
-    //       account_request.subject=account_request.subject+"on: "+dateTime();
-    //       db = database;
-    //       db.collection('account_requests').save(account_request, function(err, result){
-    //         if (err){
-    //           // console.log(err);
-    //           next(err);
-    //         }
-    //         else{
-    //           //console.log('saved to database');
-    //           res.render('confirmed-signup');
-    //         }
-    //       })
-    //
-    //     }
-    //   })
-    // }
+
+    //validate the params
+    req.checkBody("user_name", "Please enter a valid name.").isLength({min:1,max:50});
+    req.checkBody("user_email", "Please enter a valid email address.").isEmail();
+    req.checkBody("referral_source", "Please enter a valid referral source.").optional().isLength({min:1,max:600});
+
+    var errors = req.validationErrors();
+    //redirect to confirmation page or kick out an error
+    if (errors) {
+      res.json({"status":"failure","redirect_to":'failed-signup'});
+
+      //need actual error logging here
+      console.log("encountered problems: "+errors[0].msg);
+      return;
+    } else {
+      //sanitize the submitted params, send reminder to admin
+      //and push info about the user to a database
+      console.log(req.body);
+
+      var name = sanitizer.sanitize(req.body.user_name);
+      var email = req.body.user_email;
+      var referral_source = sanitizer.sanitize(req.body.referral_source);
+      //some basic injection protection need to learn more about this topic
+      referral_source = referral_source.replace("$","DOLLAR_SIGN");
+
+      // //email reminder to admin
+      var text_to_email = name + " at "+ email+" requests an invite.\n Referral source is: "+ referral_source;
+      if(req.body.email_signup){
+        text_to_email=text_to_email + "\n This user needs to be added to the email list\n";
+      }
+      var account_request = {'subject':'Requesting an Account Invite','text':text_to_email};
+      mailer.sendAdminEmail(account_request);
+
+      // //log to database
+      MongoClient.connect(process.env.LINK_TO_MONGO, function(err, database){
+        if (err){
+          // console.log(err);
+          next(err);
+        }
+        else{
+          account_request.subject=account_request.subject+" on: "+dateTime();
+          db = database;
+          db.collection('account_requests').save(account_request, function(err, result){
+            if (err){
+              console.log(err);
+              next(err);
+            }
+            else{
+              //console.log('saved to database');
+              res.json({"status":"success","redirect_to":'confirmed-signup'});
+            }
+          })
+
+        }
+      })
+    }
 });
 
 router.get('/confirmed-signup',
   function(req, res){
     res.render('confirmed-signup',{ title: 'INFINITE.INDUSTRIES: Thank you!' });
+  });
+
+router.get('/failed-signup',
+  function(req, res){
+    res.render('failed-signup',{ title: 'INFINITE.INDUSTRIES: Hmmmmm, something went wrong :(' });
   });
 //----------------------------------
 router.get('/login',
@@ -92,7 +106,7 @@ router.get('/logout',
 
 router.get('/login-error',
   function(req, res){
-    res.render('login-error');
+    res.render('login-error',{ title: 'INFINITE.INDUSTRIES: Hmmmmm, something went wrong :(' });
   });
 
 module.exports = router;
