3b01bee99a93d11432390111b9410e8d361665f7
dima <shifting.planes@gmail.com>
2016-08-21 20:00:07 -0400

    basic tune up of catalog

--- routes/artworks.js
+++ routes/artworks.js
@@ -4,14 +4,14 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
 var controller = require('../controllers.js');
 
-router.get('/catalog', ensureLoggedIn, function(req, res, next) {
+router.get('/catalog',/* ensureLoggedIn, */function(req, res, next) {
   //console.log(controller.CATALOG());
   var catalog_info = controller.CATALOG();
   catalog_info["title"] = "CATALOG";
   res.render('artworks/catalog', catalog_info );
 });
 
-router.get('/artwork/:release_type/:which_work', ensureLoggedIn, function(req, res, next) {
+router.get('/artwork/:release_type/:which_work',/* ensureLoggedIn,*/ function(req, res, next) {
   //console.log(req.params.which_work);
   var work_info = controller.ARTWORK(req.params.release_type,req.params.which_work);
   if(work_info.status=="success"){
